175ad472335dd8b41183d8fd1f89e081c1caa546
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-07-20 21:33:11 +0530

    JS start

---
+++ intro.js
@@ -0,0 +1,12 @@
+//Print
+console.log("Hello JavaScript :)");
+//Declare variable
+let a; 
+//Undefined
+a = 10;
+a = 10.1;
+a = "I am a string";
+a = 'I am also a string'
+console.log(a)
+//variable types : two types :- 1) Primitive 2) Non-Primitive
+// Primitive types : number, 
